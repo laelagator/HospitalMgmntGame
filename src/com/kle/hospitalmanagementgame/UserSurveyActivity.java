ac4ed5eaa246de12804128f47f30463c6e9f0544
laelagator <laelagator@users.noreply.github.com>
2014-08-15 10:37:58 -0500

    Reference to new classes

--- src/com/kle/hospitalmanagementgame/UserSurveyActivity.java
+++ src/com/kle/hospitalmanagementgame/UserSurveyActivity.java
@@ -1,21 +1,13 @@
 package com.kle.hospitalmanagementgame;
 
-import com.kle.hospitalmanagementgame.R;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class UserSurveyActivity extends Activity{
 
