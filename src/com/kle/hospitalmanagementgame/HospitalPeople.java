18f2290e3bc15bd7c7cf739908dd6dc4adc50778
laelagator <laelagator@users.noreply.github.com>
2014-08-15 10:39:46 -0500

    New class

---
+++ src/com/kle/hospitalmanagementgame/HospitalPeople.java
@@ -0,0 +1,15 @@
+package com.kle.hospitalmanagementgame;
+
+public class HospitalPeople {
+
+	public static int patientA = 1;
+	public static int patientB = 0;
+	public static int doctors = 3;
+	public static int nurses = 3;
+	public static int surgeons = 3;
+	public static int npcDoctors = 3;
+	public static int npcNurses = 3;
+	public static int npcSurgeons = 3;
+	public static int npcPatientA = 0;
+	public static int npcPatientB = 1;
+}
